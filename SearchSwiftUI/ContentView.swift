cfc571e74eabb6cae3f66e4697f93053933c3b43
Levi Lunique <levi.lunique@gmail.com>
2021-11-29 09:09:43 -0500

    Initial Commit

---
+++ SearchSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SearchSwiftUI
+//
+//  Created by user204006 on 11/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SearchSwiftUI/SearchSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchSwiftUIApp.swift
+//  SearchSwiftUI
+//
+//  Created by user204006 on 11/29/21.
+//
+
+import SwiftUI
+
+@main
+struct SearchSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
